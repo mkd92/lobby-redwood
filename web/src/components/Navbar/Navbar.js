63adfa2510770fd5df052ade9cafedce691c9183
mkd92 <manikandan.m1116@gmail.com>
2022-07-08 10:45:27 +0530

    navbar implemented

--- web/src/components/Navbar/Navbar.js
+++ web/src/components/Navbar/Navbar.js
@@ -1,8 +1,35 @@
+import { AppBar, Toolbar, Typography, Box, Button } from '@mui/material'
 const Navbar = () => {
   return (
     <div>
-      <h2>{'Navbar'}</h2>
-      <p>{'Find me in ./web/src/components/Navbar/Navbar.js'}</p>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <Typography variant="h6">Lobby</Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <Button
+            sx={{
+              color: 'white',
+              borderColor: 'white',
+              display: 'block',
+              mx: '.5rem',
+            }}
+            variant="outlined"
+          >
+            Login
+          </Button>
+          <Button
+            sx={{
+              color: 'white',
+              borderColor: 'white',
+              display: 'block',
+              mx: '.5rem',
+            }}
+            variant="outlined"
+          >
+            Signup
+          </Button>
+        </Toolbar>
+      </AppBar>
     </div>
   )
 }
